066e5417bd0218fc202dec083d3c6986cb5a1fd9
Steven <reversescale@icloud.com>
2018-05-04 10:14:23 +0800

    🚚:Update the upload configuration file regularly

---
+++ Example/Pods/Target Support Files/RSCheckBoxLib/RSCheckBoxLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RSCheckBoxLib : NSObject
+@end
+@implementation PodsDummy_RSCheckBoxLib
+@end
---
+++ Example/Pods/Target Support Files/RSCheckBoxLib/RSCheckBoxLib-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ZZCheckBox.h"
+#import "ZZCheckBoxButton.h"
+#import "ZZCheckBoxHeader.h"
+#import "ZZMutableCheckBox.h"
+#import "ZZSingleCheckBox.h"
+
+FOUNDATION_EXPORT double RSCheckBoxLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char RSCheckBoxLibVersionString[];
+
